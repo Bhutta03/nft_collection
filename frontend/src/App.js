5bea736e32b1f06b1b095443cea2933c2f8418af
Bhutta03 <khubaibishtiaq882@gmail.com>
2023-10-27 12:17:47 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+const nearAPI = require('near-api-js');
+
+async function mintNFT() {
+    const config = {
+    networkId: 'default',
+    nodeUrl: 'https://rpc.testnet.near.org',
+    walletUrl: 'https://wallet.testnet.near.org',
+    helperUrl: 'https://helper.testnet.near.org',
+    explorerUrl: 'https://explorer.testnet.near.org',
+    };  // Network configuration
+
+    const keyStore = new nearAPI.keyStores.UnencryptedFileSystemKeyStore('\\wsl.localhost\\Ubuntu-22.04\\home\\khubaib\\.near-credentials');
+    const near = await nearAPI.connect({...config, keyStore});
+    const account = await near.account('bhuttag.testnet');
+
+    const contract = new nearAPI.Contract(account, 'bhuttag.testnet', {
+        viewMethods: [],
+        changeMethods: ['mint_nft'],
+        sender: 'bhuttag.testnet',
+    });
+
+    const metadata = {
+        title: 'My NFT Title',
+        description: 'My NFT Description',
+        image: 'https://example.com/my-nft-image.png',
+    };
+
+    await contract.mint_nft({ token_id: 'token1', metadata });
+}
+
+mintNFT().catch(console.error);
